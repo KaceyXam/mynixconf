d245af41298e2d01cbc7e2d7d1c30da2bb094fe1
Kacey Kniffen <kaceykniffen@gmail.com>
2026-03-25 19:08:37 -0500

    Use local fork

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     import-tree.url = "github:vic/import-tree";
 
     wrappers.url = "github:Lassulus/wrappers";
-    wrapper-modules.url = "github:KaceyXam/nix-wrapper-modules";
+    wrapper-modules.url = "/home/kxkniffen/Documents/Projects/nix-wrapper-modules";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
