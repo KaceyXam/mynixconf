585c23084d3eb76ea6fbe51c84c566363360b1a7
Kacey Kniffen <kaceykniffen@gmail.com>
2026-07-26 23:48:30 -0500

    Pinned xdg-desktop-portal-wlr to an old version since the devs released a broken version

--- flake.nix
+++ flake.nix
@@ -6,17 +6,27 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixpkgs-old.url = "github:nixos/nixpkgs/46db2e09e1d3f113a13c0d7b81e2f221c63b8ce9";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, nixpkgs-old, home-manager, ... }:
   let
     system = "x86_64-linux";
+    pkgs-old = import nixpkgs-old { inherit system; };
+    overlays = [
+      (final: prev: {
+        xdg-desktop-portal-wlr = pkgs-old.xdg-desktop-portal-wlr;
+      })
+    ];
   in {
     nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
       inherit system;
 
       modules = [
         ./hosts/laptop/config.nix
+
+        { nixpkgs.overlays = overlays; }
 
         home-manager.nixosModules.home-manager
 
@@ -29,6 +39,8 @@
 
       modules = [
         ./hosts/desktop/config.nix
+
+        { nixpkgs.overlays = overlays; }
 
         home-manager.nixosModules.home-manager
 
